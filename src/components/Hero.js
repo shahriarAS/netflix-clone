f4952504098e6a260dfc63be83e60cfec922296e
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2020-11-02 20:38:18 +0600

    Hero Button and row padding fixed in mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,15 @@ function Hero(props) {
     const {title, overview} = props
     return (
         <div className="hero">
-            <div className="absolute hero_text pl-8 text-white">
+            <div className="absolute hero_text pl-4 md:pl-8 text-white">
                 <h1 className="text-white font-bold text-4xl">{title}</h1>
-                <div className="flex gap-4 mt-4">
+                <div className="flex flex-col md:flex-row mx-auto w-32 md:w-full gap-4 mt-4">
                     <button
-                        className="btn-modal px-12 text-sm md:text-lg font-bold py-2 hover:bg-white hover:text-black">
+                        className="btn-modal md:px-12 text-sm md:text-lg font-bold py-2 hover:bg-white hover:text-black">
                         <i className="fa fa-play mr-2"></i> Play
                         </button>
                     <button
-                        className="btn-modal px-12 text-sm md:text-lg font-bold py-2 hover:bg-white hover:text-black">
+                        className="btn-modal md:px-12 text-sm md:text-lg font-bold py-2 hover:bg-white hover:text-black">
                         <i className="fa fa-list mr-2"></i> My List
                         </button>
                 </div>
